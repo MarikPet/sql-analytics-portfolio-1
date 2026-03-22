9286a52a9bc25e39a3376e61cce2ca0fbad8cf8c
MarikPet <marikpetros@gmail.com>
2026-03-22 12:33:16 +0400

    Modify window functions file

--- queries/window_functions_on_analytics.sql
+++ queries/window_functions_on_analytics.sql
@@ -1,3 +1,4 @@
+-- Aggregation window functions
 SELECT
 	 o.order_id,
 	 customer_id,
@@ -7,7 +8,7 @@ JOIN analytics.order_items oi ON o.order_id = oi.order_id
 JOIN analytics.products p ON oi.product_id = p.product_id
 ;
 
-
+-- Rank Window functions
 SELECT
 	 o.order_id,
 	 customer_id,
@@ -20,6 +21,7 @@ JOIN analytics.order_items oi ON o.order_id = oi.order_id
 JOIN analytics.products p ON oi.product_id = p.product_id
 ;
 
+-- Value from another row Window functions
 SELECT
 	 o.order_id,
 	 customer_id,
@@ -46,6 +48,7 @@ JOIN analytics.cities ci ON c.city_id = ci.city_id
 GROUP BY city_name, c.customer_id
 ;
 
+-- String aggregation window
 SELECT
 	CONCAT(first_name, ' ', last_name) AS customer,
 	order_date,
@@ -56,4 +59,62 @@ FROM analytics.orders o
 JOIN analytics.order_items oi ON o.order_id = oi.order_id
 JOIN analytics.customers c ON o.customer_id = c.customer_id 
 JOIN analytics.products p ON oi.product_id = p.product_id
+;
+
+
+CREATE TEMP TABLE tmp_sales AS
+SELECT *
+FROM (
+    VALUES
+        
+        (1,  'A', DATE '2024-01-01', 100, 'online'),
+        (2,  'A', DATE '2024-01-02', 120, 'store'),
+        (3,  'A', DATE '2024-01-03', 90,  'online'),
+        (4,  'A', DATE '2024-01-04', 130, 'store'),
+        (4,  'A', DATE '2024-01-05', 110, 'store'),
+
+        
+        (5,  'B', DATE '2024-01-01', 180, 'store'),
+        (6,  'B', DATE '2024-01-02', 200, 'online'),
+        (7,  'B', DATE '2024-01-03', 220, 'online'),
+        (8,  'B', DATE '2024-01-04', 200, 'store'),
+
+        
+        (9,  'C', DATE '2024-01-01', 150, 'online'),
+        (10, 'C', DATE '2024-01-02', 150, 'online'),
+        (11, 'C', DATE '2024-01-03', 170, 'online'),
+
+        
+        (12, 'D', DATE '2024-01-01', 90,  'store'),
+        (13, 'D', DATE '2024-01-02', 110, 'store'),
+
+        
+        (14, 'E', DATE '2024-01-01', 140, 'store'),
+        (15, 'E', DATE '2024-01-02', 160, 'online'),
+        (16, 'E', DATE '2024-01-03', 155, 'store')
+) AS t(
+    sale_id,
+    customer_id,
+    sale_date,
+    amount,
+    channel
+);
+
+
+WITH beh_patt AS (SELECT
+	customer_id,
+	sale_date,
+	channel,
+	STRING_AGG(channel, ' -> ') OVER (
+				PARTITION BY customer_id ORDER BY sale_date
+				ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING 
+				) AS behav_pattern
+FROM tmp_sales)
+SELECT
+	customer_id,
+	behav_pattern
+FROM beh_patt
+GROUP BY
+	customer_id,
+	behav_pattern
 ;

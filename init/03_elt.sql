63ebe1d4dcfbd99cd29b69644c92aa7bb6762fbd
MarikPet <marikpetros@gmail.com>
2026-03-25 17:17:26 +0400

    Modify init/03_elt.sql

--- init/03_elt.sql
+++ init/03_elt.sql
@@ -1,5 +1,4 @@
 --- 3. Load data into the raw table
-\echo 'Loading raw data into analytics.coffee_shop_raw'
 COPY analytics.coffee_shop_raw
 FROM '/docker-entrypoint-initdb.d/data/coffee_shop/coffee-shop-sales-revenue.csv'
 CSV HEADER DELIMITER '|' 

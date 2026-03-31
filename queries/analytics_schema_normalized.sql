949a071bb9fed2e45d543ffb56624a8380f6b34c
MarikPet <marikpetros@gmail.com>
2026-03-31 21:39:23 +0400

    Geographical and Store tables

--- queries/analytics_schema_normalized.sql
+++ queries/analytics_schema_normalized.sql
@@ -1,9 +1,9 @@
--- PostGis extention for GEOMETRY
+-- PostGis extension for GEOMETRY
 DROP EXTENSION IF EXISTS postgis CASCADE;
 CREATE EXTENSION postgis;
 SELECT PostGIS_Version();
 
--- Change colomns' types from numeric to INT to save the sppace
+-- Change columns' types from numeric to INT to save the space
 ALTER TABLE analytics.coffee_shop_raw
 ALTER COLUMN store_id TYPE INT,
 ALTER COLUMN product_id TYPE INT;
@@ -12,19 +12,14 @@ ALTER COLUMN product_id TYPE INT;
 --     Creating Store Locations (Geographic hierarchy)  and Stores tables
 -----------------------------------------------------------------------------------------
 
--- Table for stores' locations
+-- Tables for stores' locations
+
+-- Spatial table (geometry type in EPSG:4326)
 DROP TABLE IF EXISTS analytics.store_locations CASCADE;
 
 CREATE TABLE analytics.store_locations (
     location_id INT PRIMARY KEY,
-    location_name VARCHAR(100) NOT NULL
-);
-
--- Spatial table (geometry type in EPSG:4326)
-DROP TABLE IF EXISTS analytics.store_location_boundaries CASCADE;
-
-CREATE TABLE analytics.store_location_boundaries (
-    location_id INT,
+	location_name VARCHAR(100) NOT NULL,
     geom GEOMETRY(Polygon, 4326)
 );
 
@@ -33,37 +28,52 @@ DROP TABLE IF EXISTS analytics._stg_store_location_boundaries CASCADE;
 
 CREATE TABLE IF NOT EXISTS analytics._stg_store_location_boundaries (
    	location_id INT,
+	location_name VARCHAR(100) NOT NULL,
     wkt TEXT
 );
 
 -- Store table
-DROP TABLE IF EXISTS analytics.store CASCADE;
+DROP TABLE IF EXISTS analytics.stores CASCADE;
 
 CREATE TABLE analytics.stores (
 	store_id INT PRIMARY KEY,
 	location_id INT REFERENCES analytics.store_locations(location_id)
 );
 
---     Populating Store Locations (Greographical hierarchy)  and Stores tables
+--     Populating Store Locations (Geographical hierarchy)  and Stores tables
 -----------------------------------------------------------------------------------------
+TRUNCATE TABLE analytics._stg_store_location_boundaries;
 
 COPY analytics._stg_store_location_boundaries
 FROM '/docker-entrypoint-initdb.d/data/analytics_schema/store_location_boundaries.csv'
 CSV HEADER;
 
 -- Convert WKT to geometry and populate spatial table
-INSERT INTO analytics.store_location_boundaries (location_id, geom)
+INSERT INTO analytics.store_locations (location_id, location_name, geom)
 SELECT
   location_id,
+  location_name,
   ST_GeomFromText(wkt, 4326)
 FROM analytics._stg_store_location_boundaries;
 
 
--- INSERT INTO analytics._stg_store_location_boundaries (name, geom) VALUES 
--- 1 ('Hell''s Kitchen', ST_GeomFromText('POLYGON((-73.993 40.771, -73.984 40.765, -74.001 40.752, -74.011 40.758, -73.993 40.771))', 4326))
--- 2 ('Astoria', ST_GeomFromText('POLYGON((-73.924 40.776, -73.901 40.776, -73.905 40.758, -73.939 40.752, -73.924 40.776))', 4326))
--- 3 ('Lower Manhattan', ST_GeomFromText('POLYGON((-74.010 40.700, -73.970 40.710, -73.980 40.740, -74.020 40.730, -74.010 40.700))', 4326));
+INSERT INTO analytics.stores (store_id, location_id)
+SELECT
+	DISTINCT
+	store_id,
+  	location_id
+FROM analytics.coffee_shop_raw raw
+JOIN analytics.store_locations sl ON raw.store_location = sl.location_name;
+
 
 SELECT 
 	*
 FROM analytics._stg_store_location_boundaries;
+
+SELECT 
+	*
+FROM analytics.store_locations;
+
+SELECT 
+	*
+FROM analytics.stores;
